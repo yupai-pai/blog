7fd641a6ec34b4c57f7d2549683b8a4aa60aacac
YUPAI <53607390+yupai-pai@users.noreply.github.com>
2024-07-18 21:50:47 +0800

    init

---
+++ src/config.ts
@@ -0,0 +1,73 @@
+import type {
+  LicenseConfig,
+  NavBarConfig,
+  ProfileConfig,
+  SiteConfig,
+} from './types/config'
+import { LinkPreset } from './types/config'
+
+export const siteConfig: SiteConfig = {
+  title: 'Yu',
+  subtitle: 'Blogs',
+  lang: 'zh_CN',         // 'en', 'zh_CN', 'zh_TW', 'ja'
+  themeColor: {
+    hue: 250,         // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
+    fixed: false,     // Hide the theme color picker for visitors
+  },
+  banner: {
+    enable: true,
+    src: 'assets/images/ff-banner.jpg',   // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+    position: 'center', // Equivalent to object-position, defaults center
+  },
+  favicon: [    // Leave this array empty to use the default favicon
+    // {
+    //   src: '/favicon/icon.png',    // Path of the favicon, relative to the /public directory
+    //   theme: 'light',              // (Optional) Either 'light' or 'dark', set only if you have different favicons for light and dark mode
+    //   sizes: '32x32',              // (Optional) Size of the favicon, set only if you have favicons of different sizes
+    // }
+  ]
+}
+
+export const navBarConfig: NavBarConfig = {
+  links: [
+    LinkPreset.Home,
+    LinkPreset.Archive,
+    //LinkPreset.About,
+    //{
+      //name: 'GitHub',
+      //url: 'https://github.com/saicaca/fuwari',     // Internal links should not include the base path, as it is automatically added
+      //external: true,                               // Show an external link icon and will open in a new tab
+    //},
+  ],
+}
+
+export const profileConfig: ProfileConfig = {
+  avatar: 'assets/images/ff.jpg',  // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+  name: 'Ray',
+  bio: 'Ciallo～(∠・ω< )⌒☆',
+  links: [
+    {
+      name: 'Weixin',
+      icon: 'fa6-brands:weixin',       // Visit https://icones.js.org/ for icon codes
+                                        // You will need to install the corresponding icon set if it's not already included
+                                        // `pnpm add @iconify-json/<icon-set-name>`
+      url: 'https://raw.githubusercontent.com/yupai-pai/blogs/main/src/assets/images/weixin.jpg',
+    },
+    {
+      name: 'Steam',
+      icon: 'fa6-brands:steam',
+      url: 'https://steamcommunity.com/profiles/76561198379842906/',
+    },
+    {
+      name: 'GitHub',
+      icon: 'fa6-brands:github',
+      url: 'https://github.com/yupai-pai',
+    },
+  ],
+}
+
+export const licenseConfig: LicenseConfig = {
+  enable: true,
+  name: 'CC BY-NC-SA 4.0',
+  url: 'https://creativecommons.org/licenses/by-nc-sa/4.0/',
+}
---
+++ src/content/posts/other/创建fuwari博客教程.md
@@ -0,0 +1,28 @@
+---
+title: 创建fuwari博客教程
+published: 2024-07-18
+description: ''
+image: ''
+tags: [教程,博客]
+category: '其他'
+draft: false 
+---
+
+
+
+# 开源链接
+
+https://github.com/saicaca/fuwari    谢谢大佬们
+
+
+
+
+
+# 创建仓库
+
+登录github创建公开仓库
+
+![image-20240718213155361](./%E5%88%9B%E5%BB%BAfuwari%E5%8D%9A%E5%AE%A2%E6%95%99%E7%A8%8B.assets/image-20240718213155361.png)
+
+
+
