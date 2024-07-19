804cc80ac6048e389b111838fa1a8db6cb03938f
yusiyang <489552323@qq.com>
2024-07-19 12:49:30 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -32,7 +32,7 @@ export const navBarConfig: NavBarConfig = {
   links: [
     LinkPreset.Home,
     LinkPreset.Archive,
-    //LinkPreset.About,
+    LinkPreset.About,
     //{
       //name: 'GitHub',
       //url: 'https://github.com/saicaca/fuwari',     // Internal links should not include the base path, as it is automatically added
